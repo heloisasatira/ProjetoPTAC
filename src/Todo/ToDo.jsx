8e3d11010644b9570bb02ac3de44ef561b63fcf9
Heloisa S. Rios <131817283+heloisasatira@users.noreply.github.com>
2023-09-12 20:48:45 -0300

    Adicionada a função de coletar o nome do usuário usando UseState.
    const salvarN = (e) => {//para coletar o nome do usuário
            e.preventDefault();//previnir a ação de enviar o formulário
            setListaUsuario([...listaUsuario, {//transforma a listaUsuario em array, para objeto
                nome: nome,
                id: idUsuario //conceder o id atual
            }]);
            setIdUsuario(id + 1);
            setNome("");
            console.log(listaUsuario);
        };

--- src/Todo/ToDo.jsx
+++ src/Todo/ToDo.jsx
@@ -5,11 +5,28 @@ import { Link } from "react-router-dom";
 import "./style.css";
 
 export default function ToDo() {
-    const [atividade, setAtividade] = useState("");
+    const [atividade, setAtividade] = useState("");//definir que inicialmente atividade será uma string
 
-    const [lista, setLista] = useState([]);
+    const [lista, setLista] = useState([]);//definir que lista será um array
 
-    const [id, setId] = useState(1);
+    const [id, setId] = useState(1);// "estado" para contar os ids
+
+    const [idUsuario, setIdUsuario] = useState(1);
+    const [listaUsuario, setListaUsuario] = useState([]);//definir que atividade2 será um array
+    const [nome, setNome] = useState("");//definir que nome será um array
+
+
+    const salvarN = (e) => {//para coletar o nome do usuário
+        e.preventDefault();//previnir a ação de enviar o formulário
+        setListaUsuario([...listaUsuario, {
+            nome: nome,
+            id: idUsuario //conceder o id atual
+        }]);
+        setIdUsuario(id + 1);
+        setNome("");
+        console.log(listaUsuario);
+    };
+
 
     const salvar = (e) => {
         e.preventDefault();
@@ -33,13 +50,31 @@ export default function ToDo() {
     return (
         <div class="container">
             <Link to="/">home</Link>
-            <h1>Lista de Atividades</h1>
+
+
+            <h4>Adicione aqui o seu nome</h4>
+            <form class="form" onSubmit={salvarN}>
+                <input type="text"
+                    value={nome}
+                    onChange={(e) => { setNome(e.target.value) }} />
+                <button>nome de usuário</button>
+            </form>
+            {listaUsuario.map((user) =>
+                <ul key={user.id}>
+                    <li class="nome">
+                        <p>{user.nome}</p>
+                    </li>
+                </ul>
+           )}
+       
+            <h3>Liste aqui os seus artistas favoritos</h3>
             <form onSubmit={salvar}>
                 <input type="text"
                     value={atividade}
                     onChange={(e) => { setAtividade(e.target.value) }} />
-                <button>ADD</button>
+                <button>add</button>
             </form>
+
             {lista.map((ativ) =>
                 <ul key={ativ.id}>
                     <li>
@@ -47,7 +82,8 @@ export default function ToDo() {
                         <button onClick={() => remover(ativ.id)}>Remover</button>
                     </li>
                 </ul>
-            )}
+           )}
+
         </div>
     );
 }
